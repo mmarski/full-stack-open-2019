3859134268489bca51c1e5fe0b21b9ea1e0fb289
Teivo Markus <markus.teivo@aalto.fi>
2018-10-10 20:18:09 +0300

    Kierros1 1.3 done

---
+++ Kierros1/osa1/src/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Otsikko = (props) => (
+    <h1>{props.kurssi}</h1>
+)
+
+const Osa = (props) => (
+    <p>{props.osa.nimi} {props.osa.tehtavia}</p>
+)
+
+const Sisalto = (props) => (
+    <React.Fragment>
+        <Osa osa={props.osa1} />
+        <Osa osa={props.osa2} />
+        <Osa osa={props.osa3} />
+    </React.Fragment>
+)
+
+const Yhteensa = (props) => (
+    <p>yhteensä {props.tehtavia1 + props.tehtavia2 + props.tehtavia3} tehtävää</p>
+)
+
+const App = () => {
+    const kurssi = 'Half Stack -sovelluskehitys'
+    const osa1 = {
+        nimi: 'Reactin perusteet',
+        tehtavia: 10
+    }
+    const osa2 = {
+        nimi: 'Tiedonvälitys propseilla',
+        tehtavia: 7
+    }
+    
+    const osa3 = {
+        nimi: 'Komponenttien tila',
+        tehtavia: 14
+    }
+    return (
+        <div>
+            <Otsikko kurssi={kurssi} />
+            <Sisalto osa1={osa1} osa2={osa2} osa3={osa3} />
+            <Yhteensa tehtavia1={osa1.tehtavia} tehtavia2={osa2.tehtavia} tehtavia3={osa3.tehtavia} />
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
