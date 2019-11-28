b51b5db8ab02518e30ea4b18e35ea5aa73323e1b
Teivo Markus <markus.teivo@aalto.fi>
2019-11-28 12:34:24 +0200

    2.11 puhelinluettelo done

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,15 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 import PersonList from './PersonList';
 import FilterForm from './FilterForm';
 import PersonForm from './PersonForm';
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ nameFilter, setNameFilter ] = useState('')
@@ -44,6 +40,14 @@ const App = () => {
       setShowAll(false)
     }
   }
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const personsToShow = showAll ? persons : persons.filter(
     p => p.name.toLowerCase().includes(nameFilter.toLowerCase())
