4706316e13d69082e91463082aaa76ff4524a453
Markus Teivo <markus.teivo@aalto.fi>
2020-05-24 21:08:55 +0300

    4.2 done

---
+++ osa4/blogilista/utils/middleware.js
@@ -0,0 +1,25 @@
+const morgan = require('morgan')
+
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+const requestLogger = morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]')
+
+// olemattomien osoitteiden käsittely
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+
+// virheellisten pyyntöjen käsittely
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformed id' })
+  }
+  else if (error.name === 'ValidationError') {
+    return res.status(400).send({ error: error.message })
+  }
+
+  next(error)
+}
+
+module.exports = { requestLogger, unknownEndpoint, errorHandler }
