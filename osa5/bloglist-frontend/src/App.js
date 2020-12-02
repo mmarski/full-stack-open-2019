ebb7e2cacd143eb4f53ee7d2a4fc8372bfc43cdf
Markus Teivo <markus.teivo@aalto.fi>
2020-12-02 03:58:49 +0200

    5.2 done

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -1,30 +1,66 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import Notification from './components/Notification'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
-  const [username, setUsername] = useState('') 
-  const [password, setPassword] = useState('') 
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
-    )  
+    )
   }, [])
 
-  const handleLogin = (event) => {
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    console.log('using the effect')
+    if (loggedUserJSON) {
+      console.log('Got logged in user', loggedUserJSON)
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const handleLogin = async (event) => {
     event.preventDefault()
     console.log('logging in with', username, password)
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
+      console.log('Stringified', JSON.stringify(user))
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    }
+
+    catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
   }
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+  const handleLogout = () => {
+    setUser(null)
+    window.localStorage.clear()
+  }
 
+  const loginForm = () => (
+    <div>
       <h2>Login</h2>
       <form onSubmit={handleLogin}>
         <div>
@@ -47,6 +83,27 @@ const App = () => {
         </div>
         <button type="submit">login</button>
       </form>
+    </div>
+  )
+
+  const blogList = () => (
+    <div>
+      <h2>Blogs</h2>
+
+      <p>{user.name} logged in <button onClick={handleLogout}>Log out</button></p>
+
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+
+  return (
+    <div>
+      <Notification message={errorMessage} />
+
+      {user === null ? loginForm() : blogList()}
+
     </div>
   )
 }
---
+++ osa5/bloglist-frontend/src/components/Notification.js
@@ -0,0 +1,8 @@
+import React from 'react'
+const Notification = ({ message }) => (
+  <div>
+    {message}
+  </div>
+)
+
+export default Notification
